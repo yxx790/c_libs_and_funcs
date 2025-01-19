553aba3400b8eebc4d48fd5ee144c3aee53f12f4
yxx790 <yxx790@gmail.com>
2025-01-19 19:59:26 +0500

    first commit

--- Flash.c
+++ Flash.c
@@ -9,8 +9,7 @@
 /* USER CODE END PD */
 
 /* USER CODE BEGIN 0 */
-void erase_flash(void)
-{
+void erase_flash(void){
 	HAL_FLASH_Unlock();
 	FLASH_EraseInitTypeDef EraseInitStruct;
 	EraseInitStruct.TypeErase = FLASH_TYPEERASE_PAGES;
@@ -20,50 +19,40 @@ void erase_flash(void)
 	HAL_FLASHEx_Erase(&EraseInitStruct, &PageError);
 	HAL_FLASH_Lock();
 }
-void save_to_flash(uint8_t *data)
-{
+void save_to_flash(uint8_t *data){
 	volatile uint32_t data_to_FLASH[(strlen((char*)data)/4)	+ (int)((strlen((char*)data) % 4) != 0)];
 	memset((uint8_t*)data_to_FLASH, 0, strlen((char*)data_to_FLASH));
 	strcpy((char*)data_to_FLASH, (char*)data);
-	volatile uint32_t data_length = (strlen((char*)data_to_FLASH) / 4)
-									+ (int)((strlen((char*)data_to_FLASH) % 4) != 0);
-	volatile uint16_t pages = (strlen((char*)data)/page_size)
-									+ (int)((strlen((char*)data)%page_size) != 0);
-	  HAL_FLASH_Unlock();
-	  HAL_FLASH_OB_Unlock();
-	  FLASH_EraseInitTypeDef EraseInitStruct;
-	  EraseInitStruct.TypeErase = FLASH_TYPEERASE_PAGES;
-	  EraseInitStruct.PageAddress = FLASH_STORAGE;
-	  EraseInitStruct.NbPages = pages;
-	  uint32_t PageError;
-	  volatile uint32_t write_cnt=0, index=0;
-	  volatile HAL_StatusTypeDef status;
-	  status = HAL_FLASHEx_Erase(&EraseInitStruct, &PageError);
-	  while(index < data_length)
-	  {
-		  if (status == HAL_OK)
-		  {
-			  status = HAL_FLASH_Program(FLASH_TYPEPROGRAM_WORD, FLASH_STORAGE+write_cnt, data_to_FLASH[index]);
-			  if(status == HAL_OK)
-			  {
-				  write_cnt += 4;
-				  index++;
-			  }
-		  }
-	  }
-	  HAL_FLASH_OB_Lock();
-	  HAL_FLASH_Lock();
+	volatile uint32_t data_length = (strlen((char*)data_to_FLASH) / 4) + (int)((strlen((char*)data_to_FLASH) % 4) != 0);
+	volatile uint16_t pages = (strlen((char*)data)/page_size) + (int)((strlen((char*)data)%page_size) != 0);
+	HAL_FLASH_Unlock();
+	HAL_FLASH_OB_Unlock();
+	FLASH_EraseInitTypeDef EraseInitStruct;
+	EraseInitStruct.TypeErase = FLASH_TYPEERASE_PAGES;
+	EraseInitStruct.PageAddress = FLASH_STORAGE;
+	EraseInitStruct.NbPages = pages;
+	uint32_t PageError;
+	volatile uint32_t write_cnt=0, index=0;
+	volatile HAL_StatusTypeDef status;
+	status = HAL_FLASHEx_Erase(&EraseInitStruct, &PageError);
+	while(index < data_length){
+		if (status == HAL_OK){
+			status = HAL_FLASH_Program(FLASH_TYPEPROGRAM_WORD, FLASH_STORAGE+write_cnt, data_to_FLASH[index]);
+			if(status == HAL_OK){
+				write_cnt += 4;
+				index++;
+			}
+		}
+	}
+	HAL_FLASH_OB_Lock();
+	HAL_FLASH_Lock();
 }
-
-void read_flash(uint8_t* data)
-{
+void read_flash(uint8_t* data){
 	volatile uint32_t read_data;
 	volatile uint32_t read_cnt=0;
-	do
-	{
+	do{
 		read_data = *(uint32_t*)(FLASH_STORAGE + read_cnt);
-		if(read_data != 0xFFFFFFFF)
-		{
+		if(read_data != 0xFFFFFFFF){
 			data[read_cnt] = (uint8_t)read_data;
 			data[read_cnt + 1] = (uint8_t)(read_data >> 8);
 			data[read_cnt + 2] = (uint8_t)(read_data >> 16);
@@ -73,6 +62,7 @@ void read_flash(uint8_t* data)
 	}while(read_data != 0xFFFFFFFF);
 }
 /* USER CODE END 0 */
+
 
   char flash_data[50];
   memset(flash_data, 0, sizeof(flash_data));
